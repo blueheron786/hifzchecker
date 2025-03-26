6ab8ea3b198b21325d983357ea77f9bf08515de9
blueheron786 <blueheron786@gmail.com>
2025-03-26 17:56:47 -0400

    Experiments/notes on which models work

--- listener/transcribe.py
+++ listener/transcribe.py
@@ -2,8 +2,11 @@ import whisper
 import sys
 
 # Load the Whisper model.
-# Tiny, base, small, medium, and large (requires more RAM than I have).
-model = whisper.load_model("base")
+# Tiny, base, small, medium, and large
+# LARGE: requires more GPU RAM than I have
+# SMALL: gives actually pretty decent results
+# MEDIUM: is EPIC and pretty fast tbh
+model = whisper.load_model("medium")
 
 # Transcribe the audio file passed via command-line argument
 audio_file = sys.argv[1]
